42d65bd18217d4520accf7c67383f95f4095a2d7
William Gard <Olegmixa@mail.ru>
2015-07-13 22:00:51 +0300

    update favicon
    Добавлены 2 версии иконки сайта

--- application/views/main.php
+++ application/views/main.php
@@ -1,8 +1,25 @@
 <html>
 <head>
-	<!-- Иконка сайта -->
-	<link href="<?php echo URL::base(); ?>public/image/system/favicon.ico" rel="shortcut icon" type="image/x-icon">
-	
+	<!-- Иконки сайта -->
+	<link rel="apple-touch-icon" sizes="57x57" href="<?php echo URL::base(); ?>public/image/system/apple-touch-icon-57x57.png">
+	<link rel="apple-touch-icon" sizes="60x60" href="<?php echo URL::base(); ?>public/image/system/apple-touch-icon-60x60.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="<?php echo URL::base(); ?>public/image/system/apple-touch-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="<?php echo URL::base(); ?>public/image/system/apple-touch-icon-76x76.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="<?php echo URL::base(); ?>public/image/system/apple-touch-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="<?php echo URL::base(); ?>public/image/system/apple-touch-icon-120x120.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="<?php echo URL::base(); ?>public/image/system/apple-touch-icon-144x144.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="<?php echo URL::base(); ?>public/image/system/apple-touch-icon-152x152.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="<?php echo URL::base(); ?>public/image/system/apple-touch-icon-180x180.png">
+	<link rel="icon" type="image/png" href="<?php echo URL::base(); ?>public/image/system/favicon-32x32.png" sizes="32x32">
+	<link rel="icon" type="image/png" href="<?php echo URL::base(); ?>public/image/system/favicon-194x194.png" sizes="194x194">
+	<link rel="icon" type="image/png" href="<?php echo URL::base(); ?>public/image/system/favicon-96x96.png" sizes="96x96">
+	<link rel="icon" type="image/png" href="<?php echo URL::base(); ?>public/image/system/android-chrome-192x192.png" sizes="192x192">
+	<link rel="icon" type="image/png" href="<?php echo URL::base(); ?>public/image/system/favicon-16x16.png" sizes="16x16">
+	<link rel="manifest" href="<?php echo URL::base(); ?>public/image/system/manifest.json">
+	<meta name="msapplication-TileColor" content="#ff0101">
+	<meta name="msapplication-TileImage" content="/mstile-144x144.png">
+	<meta name="theme-color" content="#f72424">
+
 	<!-- Виджет для визуальных закладок -->
 	<link rel="yandex-tableau-widget" href="<?php echo URL::base(); ?>public/script/widget.json" />
 
